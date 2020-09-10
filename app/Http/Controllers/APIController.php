8b7f7e1f76040a2145bed4037b471f0a55d89a07
lawrencechen0921 <show99520show99520@gmaul.com>
2020-09-10 18:13:08 +0800

    fisrt commit

---
+++ app/Http/Controllers/APIController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use JWTAuth;
+use App\User;
+use Illuminate\Http\Request;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use App\Http\Requests\RegistrationFormRequest;
+
+
+class APIController extends Controller
+{
+    public $loginAfterSignUp = true;
+
+    /**
+     * @param Request $_REQUEST
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function login(Request $request)
+    {
+        $input = $request->only('email', 'password');
+        $token = null;
+
+        if (!$token = JWTAuth::attempt($input)){
+            return response()->json([
+                'success' => false,
+                'message' => 'Invalid Email or Password',
+            ], 401);
+        }
+
+    
+        return response()->json([
+            'success' => true,
+            'token' => $token,
+        ]);
+    }
+
+
+    public function logout(Request $request)
+    {
+        $this->validate($request, [
+            'token' => 'required'
+        ]);
+
+        try{
+            JWTAuth::invalidate($request->token);
+
+            return response()->json([
+                'success' => true,
+                'message' => 'User logged out successfully'
+            ]);
+
+        }catch (JWTException $exception){
+            return response()->json([
+                'success' => false,
+                'message' => 'Sorry, the user cannot be logged out'
+            ], 500);
+        }
+    }
+
+    public function register(RegistrationFormRequest $request)
+    {
+        $user = new User();
+        $user->name = $request->name;
+        $user->email =$request->email;
+        $user->password = bcrypt($request->password);
+        $user->save();
+
+
+        if ($this->loginAfterSignUp){
+            return $this->login($request);
+
+        }
+        
+        return response()->json([
+            'success' => true,
+            'data'  => $user
+        ], 200);
+    }
+
+}
